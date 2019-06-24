352d15f8a9b8a16914d84ec885b137ef24666754
CN0071 <cn0071@CN0071.local>
2019-06-24 09:52:21 +0100

    Updates

---
+++ precache-manifest.be5f4329601b79c8ba77b0881588191a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153a8842d535cb4c0f69909f48dc0ffd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c03c3badd9439f655fc",
+    "url": "/static/css/main.a45cd45e.chunk.css"
+  },
+  {
+    "revision": "b000fccb79e89af31dc4",
+    "url": "/static/js/2.abf43ca4.chunk.js"
+  },
+  {
+    "revision": "0c03c3badd9439f655fc",
+    "url": "/static/js/main.5f6d29b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
