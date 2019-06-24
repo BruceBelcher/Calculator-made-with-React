b3d91f02e1e286f4218320bd0bc0ecfd52e98098
CN0071 <cn0071@CN0071.local>
2019-06-24 10:07:03 +0100

    Updates

---
+++ precache-manifest.04817ad20f77d5fb7c45d718888a051b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ddbd52497332ade4d31473386cf6363",
+    "url": "/https://github.com/BruceBelcher/Calculator-made-with-React/index.html"
+  },
+  {
+    "revision": "2156379401d0b8fa61a0",
+    "url": "/https://github.com/BruceBelcher/Calculator-made-with-React/static/css/main.a45cd45e.chunk.css"
+  },
+  {
+    "revision": "b000fccb79e89af31dc4",
+    "url": "/https://github.com/BruceBelcher/Calculator-made-with-React/static/js/2.abf43ca4.chunk.js"
+  },
+  {
+    "revision": "2156379401d0b8fa61a0",
+    "url": "/https://github.com/BruceBelcher/Calculator-made-with-React/static/js/main.26e9adf2.chunk.js"
+  },
+  {
+    "revision": "95125af01faac7e5989b",
+    "url": "/https://github.com/BruceBelcher/Calculator-made-with-React/static/js/runtime~main.5a319637.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be5f4329601b79c8ba77b0881588191a.js"
+  "/https://github.com/BruceBelcher/Calculator-made-with-React/precache-manifest.04817ad20f77d5fb7c45d718888a051b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https://github.com/BruceBelcher/Calculator-made-with-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
